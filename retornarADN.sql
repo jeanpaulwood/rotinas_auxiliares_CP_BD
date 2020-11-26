a827460c90518bb9c57d4f2478f332755e7c7d60
jeanpaulwood <jeanpaulwood08@gmail.com>
2020-11-26 12:18:45 -0300

    retornarADN - prod

--- retornarADN.sql
+++ retornarADN.sql
@@ -1,7 +1,13 @@
+
+GO
+
+/****** Object:  UserDefinedFunction [dbo].[retornarADN]    Script Date: 26/11/2020 11:19:37 ******/
 SET ANSI_NULLS ON
 GO
+
 SET QUOTED_IDENTIFIER ON
 GO
+
 
 ALTER FUNCTION [dbo].[retornarADN] (@funcicodigo int, @inicionoturno datetime,@fimnoturno datetime, @fatornoturno float, @cartadata datetime, @estendenoturno bit)
  
@@ -20,6 +26,7 @@ BEGIN
 	DECLARE @adn int,@minutos int
 	DECLARE @interval1 int = 0, @interval2 int = 0, @interval3 int = 0, @interval4 int = 0
 	DECLARE @periodo1 int = 0, @periodo2 int = 0, @periodo3 int = 0
+	DECLARE @cartacodigo int = (select top 1 cartacodigo from tbgabcartaodeponto (nolock) where funcicodigo = @funcicodigo and cartadatajornada = @cartadata)
 
 	if @inicionoturno is not null and @fimnoturno is not null
 	begin
@@ -28,17 +35,17 @@ BEGIN
 		set @interval3 = (select coalesce(adn,0) from dbo.retornarInterval3_ADN(@funcicodigo,@inicionoturno,@fimnoturno,@fatornoturno,@cartadata,@estendenoturno))
 		set @interval4 = (select coalesce(adn,0) from dbo.retornarInterval4_ADN(@funcicodigo,@inicionoturno,@fimnoturno,@fatornoturno,@cartadata,@estendenoturno))
 
-		if (select contabiliza from dbo.retornarIntervalosFuncionario(@funcicodigo,@cartadata,1) where contabiliza = 1) is not null
+		if (select contabiliza from dbo.retornarIntervalosFuncionario(@cartacodigo,1) where contabiliza = 1) is not null
 		begin
 			set @periodo1 = (select coalesce(adn,0) from dbo.retornarPeriodo1_ADN(@funcicodigo,@inicionoturno,@fimnoturno,@fatornoturno,@cartadata,@estendenoturno))
 		end
 
-		if (select contabiliza from dbo.retornarIntervalosFuncionario(@funcicodigo,@cartadata,2) where contabiliza = 1) is not null
+		if (select contabiliza from dbo.retornarIntervalosFuncionario(@cartacodigo,2) where contabiliza = 1) is not null
 		begin
 			set @periodo2 = (select coalesce(adn,0) from dbo.retornarPeriodo2_ADN(@funcicodigo,@inicionoturno,@fimnoturno,@fatornoturno,@cartadata,@estendenoturno))
 		end
 
-		if (select contabiliza from dbo.retornarIntervalosFuncionario(@funcicodigo,@cartadata,3) where contabiliza = 1) is not null
+		if (select contabiliza from dbo.retornarIntervalosFuncionario(@cartacodigo,3) where contabiliza = 1) is not null
 		begin
 			set @periodo3 = (select coalesce(adn,0) from dbo.retornarPeriodo3_ADN(@funcicodigo,@inicionoturno,@fimnoturno,@fatornoturno,@cartadata,@estendenoturno))
 		end
@@ -50,17 +57,17 @@ BEGIN
 		set @interval3 = (select coalesce(minutos,0) from dbo.retornarInterval3_ADN(@funcicodigo,@inicionoturno,@fimnoturno,@fatornoturno,@cartadata,@estendenoturno))
 		set @interval4 = (select coalesce(minutos,0) from dbo.retornarInterval4_ADN(@funcicodigo,@inicionoturno,@fimnoturno,@fatornoturno,@cartadata,@estendenoturno))
 
-		if (select contabiliza from dbo.retornarIntervalosFuncionario(@funcicodigo,@cartadata,1) where contabiliza = 1) is not null
+		if (select contabiliza from dbo.retornarIntervalosFuncionario(@cartacodigo,1) where contabiliza = 1) is not null
 		begin
 			set @periodo1 = (select coalesce(minutos,0) from dbo.retornarPeriodo1_ADN(@funcicodigo,@inicionoturno,@fimnoturno,@fatornoturno,@cartadata,@estendenoturno))
 		end
 
-		if (select contabiliza from dbo.retornarIntervalosFuncionario(@funcicodigo,@cartadata,2) where contabiliza = 1) is not null
+		if (select contabiliza from dbo.retornarIntervalosFuncionario(@cartacodigo,2) where contabiliza = 1) is not null
 		begin
 			set @periodo2 = (select coalesce(minutos,0) from dbo.retornarPeriodo2_ADN(@funcicodigo,@inicionoturno,@fimnoturno,@fatornoturno,@cartadata,@estendenoturno))
 		end
 
-		if (select contabiliza from dbo.retornarIntervalosFuncionario(@funcicodigo,@cartadata,3) where contabiliza = 1) is not null
+		if (select contabiliza from dbo.retornarIntervalosFuncionario(@cartacodigo,3) where contabiliza = 1) is not null
 		begin
 			set @periodo3 = (select coalesce(minutos,0) from dbo.retornarPeriodo3_ADN(@funcicodigo,@inicionoturno,@fimnoturno,@fatornoturno,@cartadata,@estendenoturno))
 		end
@@ -78,3 +85,4 @@ BEGIN
 	RETURN;
 END
 GO
+
