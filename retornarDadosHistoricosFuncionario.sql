d2018d133a3d5de3a25fbdc58ecbde18d664e383
jeanpaulwood <jeanpaulwood08@gmail.com>
2020-12-01 21:31:29 -0300

    Inclusao_retornarDadosFuncionarios

---
+++ retornarDadosHistoricosFuncionario.sql
@@ -0,0 +1,153 @@
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+
+-- =============================================
+-- Author:		Jean Paul
+-- Create date: 20/05/2019
+-- Description:	Trás os dados históricos do funcionário
+-- =============================================
+ALTER FUNCTION [dbo].[retornarDadosHistoricosFuncionario] 
+(
+	-- Add the parameters for the function here
+	@funcicodigo bigint,
+    @startDate datetime,		
+    @endDate datetime	
+)
+RETURNS 
+@tabela TABLE 
+(
+  dtformat varchar(10),
+  dia smallint,
+  dt date,
+  codigo_h bigint,
+  indicacao varchar(50),
+  cod_escala bigint,
+  centccodigo bigint,
+  feriado bit,
+  feriatipo char(1),
+  acordcodigo bigint,
+  cargocodigo bigint,
+  tpapocodigo int,
+  flagocorrencia bit,
+  regime int
+)
+AS
+BEGIN
+		declare @dt date;
+		declare @dia smallint;
+		declare @dtformat varchar(10);
+		declare @posterior int = 0;
+		declare @cod_escala bigint;
+		declare @cod_cc bigint;
+		declare @codigo_h bigint;
+		declare @indicacao varchar(50);
+		declare @feriado int;				-- CONTADOR DE FERIADOS 
+		declare @feriatipo char(1);
+		declare @dateini date
+		declare @datefim date;
+		declare @deduzferiado bit;
+		declare @acordcodigo bigint;
+		declare @cargocodigo bigint;
+		declare @tpapocodigo int;
+		declare @flagocorrencia bit = 0;	-- INDICA QUE EXISTE UMA OCORRÊNCIA PARA O FUNCIONÁRIO NO DIA
+		declare @regime int; -- INDICA O REGIME DA ESCALA (1 DIARISTA, 2 PLANTONISTA)
+
+		declare dias cursor FAST_FORWARD FORWARD_ONLY READ_ONLY for
+		with dateRange as
+		(
+		  select dt = @startDate
+		  where @startDate < @endDate
+		  union all
+		  select dateadd(dd, 1, dt)
+		  from dateRange
+		  where dateadd(dd, 1, dt) <= @endDate
+		)
+		select 
+		dt,
+		DATEPART(dw,dt) as dia,
+		convert(varchar(MAX),dt,103) from dateRange OPTION (MAXRECURSION 1000)
+
+		open dias
+			fetch next from dias into @dt,@dia,@dtformat
+			while @@FETCH_STATUS=0
+			begin
+
+				--RECUPERA A ESCALA DE DETERMINADA DATA
+				set @cod_escala = (select top 1 escalcodigo from tbgabfuncionarioescala (nolock) 
+									where funcicodigo = @funcicodigo and fuescdatainiciovigencia <= @dt order by fuescdatainiciovigencia desc, fuescdatamovimentacao desc)
+
+				-- RECUPERA O REGIME DA ESCALA
+				set @regime = (select top 1 escalregime from tbgabescala (nolock) where escalcodigo = @cod_escala)
+
+				--RECUPERA O HORÁRIO  E INDICAÇÃO DE DETERMINADA DATA  
+				select  @codigo_h = codigohorario,  @indicacao = indicacao from cjRetornarHorarioEscala (@cod_escala,@dt,@dt)
+
+				--RECUPERA O CENTRO DE CUSTO DE DETERMINADA DATA
+				set @cod_cc = (select dbo.retornarCentroCustoPorData(@dt,@funcicodigo))
+				if @cod_cc <> 0
+				begin
+					set @cargocodigo = (select top 1 cargocodigo from tbgabcentrocustofuncionario (nolock) where funcicodigo = @funcicodigo and cenfudatainicio <= @dt order by cenfudatainicio desc)
+				end
+				else
+				begin
+					set @cargocodigo = 0
+				end
+
+				--RECUPERA O ACORDO COLETIVO DE DETERMINADA DATA
+				set @acordcodigo = (select dbo.retornarAcordoPorData(@dt,@funcicodigo))
+
+				--RECUPERA O TIPO DE ENTRADA DE DADOS DE DETERMINADA DATA
+				set @posterior = (select count(entfucodigo) from tbgabentradaapontamentofuncionario (nolock) where funcicodigo = @funcicodigo and entfudatavinculacao > @dt)
+
+				if @posterior > 0
+				begin
+					set @tpapocodigo = (select top 1 tpapocodigo from tbgabentradaapontamentofuncionario (nolock) where funcicodigo = @funcicodigo and entfudatavinculacao <= @dt order by entfudatavinculacao desc)
+				end
+				else
+				begin
+					set @tpapocodigo = (select top 1 tpapocodigo from tbgabentradaapontamentofuncionario (nolock) where funcicodigo = @funcicodigo order by entfudatavinculacao desc)
+				end
+
+				-- VERIFICA SE PARA DETERMINADA ESCALA, O FERIADO É DEDUZIDO OU NÃO
+				set @deduzferiado = (select top 1 fuescdeduzferiado from tbgabfuncionarioescala (nolock) where funcicodigo = @funcicodigo and escalcodigo = @cod_escala) 
+
+				set @feriado = (select count(F.feriacodigo) from tbgabcentrocusto CC (nolock) 
+				inner join tbgabtabelaferiadoferiado TF (nolock) on CC.fertbcodigo=tf.fertbcodigo
+				inner join tbgabferiado F (nolock) on TF.feriacodigo=F.feriacodigo
+				where CC.centccodigo = @cod_cc and F.feriames = DATEPART(month,@dt) and F.feriadia = DATEPART(day,@dt))
+
+			--	set @indicacao = (select indicacao from cjRetornarHorarioEscala (@cod_escala,@dt,@dt))     -- FOI OTIMIZADO NO SELECT DE RECUPERACAO DE HORARIO E INDICACAO
+
+				-- QUANDO DEDUZIR FERIADO INDICA QUE O FUNCIONÁRIO TRABALHA NO REGIME DIARISTA, CASO AO CONTRÁRIO PLANTONISTA.
+				if @feriado > 0 --and @deduzferiado = 1
+				begin
+					set @feriado = 1;
+					--set @indicacao = 'Feriado'
+					set @feriatipo = (select top 1 F.fertpcodigo
+					from tbgabcentrocusto CC (nolock) 
+					inner join tbgabtabelaferiadoferiado TF (nolock) on CC.fertbcodigo=tf.fertbcodigo
+					inner join tbgabferiado F (nolock) on TF.feriacodigo=F.feriacodigo
+					where CC.centccodigo = @cod_cc and F.feriames = DATEPART(month,@dt) and F.feriadia = DATEPART(day,@dt))
+				end
+				else
+				begin
+					set @feriado = 0;
+					set @feriatipo = '';
+				end
+
+				-- VERIFICA SE PARA O DIA HÁ UMA OU MAIS OCORRÊNCIAS
+				set @flagocorrencia = (select case when count(ocorrcodigo) > 0 then 1 else 0 end from tbgabocorrencia where funcicodigo = @funcicodigo and convert(date,ocorrinicio) = @dt)
+
+				insert into @tabela values (@dtformat,@dia,@dt,@codigo_h,@indicacao,@cod_escala,@cod_cc,@feriado,@feriatipo,
+				@acordcodigo,@cargocodigo,@tpapocodigo,@flagocorrencia,@regime)
+			
+				fetch next from dias into @dt,@dia,@dtformat
+			end
+		close dias
+		deallocate dias
+	RETURN 
+END
+GO
